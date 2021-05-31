735ebc39c9db24fffc5263ca9a86f88094787c52
wanglongxun <wanglongxun@bytedance.com>
2021-05-31 18:49:12 +0800

    第三题继续
    Change-Id: Ic41e95f32ad9d0fb9803b780ee2922911e8e2386

--- app/src/main/java/com/wlx/androidfinalexample/ex3/ContactAdapter.kt
+++ app/src/main/java/com/wlx/androidfinalexample/ex3/ContactAdapter.kt
@@ -1,4 +1,40 @@
 package com.wlx.androidfinalexample.ex3
 
-class ContactAdapter() {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.wlx.androidfinalexample.R
+
+class ContactAdapter(context: Context, private val resourceId: Int, data: List<ContactItem>) :
+    ArrayAdapter<ContactItem>(context, resourceId, data) {
+    inner class ViewHolder {
+        lateinit var name: TextView
+        lateinit var phoneNumber: TextView
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val contactItem = getItem(position)
+        lateinit var view: View
+        lateinit var viewHolder: ViewHolder
+        if (convertView == null) {
+            view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+            viewHolder = ViewHolder()
+            viewHolder.name = view.findViewById(R.id.name)
+            viewHolder.phoneNumber = view.findViewById(R.id.number)
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
+        if (contactItem != null) {
+            viewHolder.phoneNumber.text = contactItem.phoneNumber
+            viewHolder.name.text = contactItem.name
+        }
+
+        return view
+    }
+
 }
--- app/src/main/java/com/wlx/androidfinalexample/ex3/Ex3Activity.kt
+++ app/src/main/java/com/wlx/androidfinalexample/ex3/Ex3Activity.kt
@@ -1,14 +1,37 @@
 package com.wlx.androidfinalexample.ex3
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.wlx.androidfinalexample.R
 
 class Ex3Activity : AppCompatActivity() {
+    private val TAG = "Ex3Activity"
+    private lateinit var contractList: ArrayList<ContactItem>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ex3)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d(TAG, "onCreate: ")
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.READ_CONTACTS), 1
+            )
+        }
+//        contractList = getContact()
+//        val listView: ListView = findViewById(R.id.list)
+//        val adapter = ContactAdapter(applicationContext, R.layout.ex3_list_item, contractList)
+//        listView.adapter = adapter
+//        listView.setOnItemClickListener { parent, view, position, id ->
+//            val contract = contractList[position]
+//        }
     }
 
     private fun getContact(): ArrayList<ContactItem> {
@@ -38,4 +61,26 @@ class Ex3Activity : AppCompatActivity() {
         }
         return contactList
     }
+
+    private fun getEmail() {
+
+    }
+
+    private fun showDialog() {
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED && requestCode == 1) {
+            contractList = getContact()
+        } else {
+            Toast.makeText(this, "获取联系人权限失败", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
